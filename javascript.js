6fe1bd327469bcdf2b378b9ae7ec135176017c58
fiolet <52757372+sarah-lishin@users.noreply.github.com>
2020-08-24 11:42:22 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
-/* Open when someone clicks on the span element */
+/* Open */
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.height = "100%";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
+/* Close */
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myNav").style.height = "0%";
+} 
